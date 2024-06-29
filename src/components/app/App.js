8767712e353a5683e18eb66cb35754f64636dd29
valeriya <valeriyastoyan26@gmail.com>
2024-06-29 16:51:23 +0300

    add SearchPanel and corr

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,7 @@
 import {Component} from 'react';
 
 import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 
 import './app.css';
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="app">
       <AppInfo/>
+      <SearchPanel/>
       <AppFilter/>
     </div>
   );
