1379dc8b17a93392f66c02ca1197a841f2e41c4a
valeriya <valeriyastoyan26@gmail.com>
2024-06-30 13:46:52 +0300

    add dinamik like" class

--- src/components/task-list-item/task-list-item.js
+++ src/components/task-list-item/task-list-item.js
@@ -1,30 +1,62 @@
+import { Component } from 'react';
+
 import './task-list-item.css';
 
-const TaskListItem = ({task, date, priority}) => {
-
-    let classNames = 'list-group-item d-flex justify-content-between';
-    if (priority) {
-        classNames += ` increase`;
+class TaskListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            priority: false,
+            process: false
+        }
     }
 
-    return (
-        <li className={classNames}>
-            <span className="list-group-item-label">{task}</span>
-            <input type="text" className="list-group-item-input" defaultValue={date}/>
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                    className="btn-cookie btn-sm ">
-                    <i className="fas fa-cookie"></i>    
-                </button>
+    onPriority = () => {
+        this.setState(({priority}) => ({
+            priority: !priority
+        }))
+    }
 
-                <button type="button"
-                    className="btn-trash btn-sm ">
-                    <i className="fas fa-trash"></i>    
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    )
+    onProcess = () => {
+        this.setState(({process}) => ({
+            process: !process
+        }))
+    }
+
+    render() {
+    const {task, date} = this.props;
+    const {priority, process} = this.state;
+
+        let classNames = 'list-group-item d-flex justify-content-between';
+        if (priority) {
+            classNames += ` increase`;
+        }
+
+        if (process) {
+            classNames += ` like`;
+        }
+
+    
+        return (
+            <li className={classNames}>
+                <span className="list-group-item-label" onClick={this.onProcess}>{task}</span>
+                <input type="text" className="list-group-item-input" defaultValue={date}/>
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button type="button"
+                        className="btn-cookie btn-sm "
+                        onClick={this.onPriority}>
+                        <i className="fas fa-cookie"></i>    
+                    </button>
+    
+                    <button type="button"
+                        className="btn-trash btn-sm ">
+                        <i className="fas fa-trash"></i>    
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        )
+    }
 }
 
 export default TaskListItem;
