d84d20bcf66ecb06ee654c433c61f9bbe31582be
valeriya <valeriyastoyan26@gmail.com>
2024-07-05 20:16:51 +0300

    change to Class, add state

--- src/components/task-add-panel/task-add-form.js
+++ src/components/task-add-panel/task-add-form.js
@@ -1,23 +1,49 @@
+import { Component } from 'react';
+
 import './task-add-form.css';
 
-const TaskAddForm = () => {
-    return (
-        <div className="app-add-form">
-            <h3>Добавьте новую задачу</h3>
-            <form
-                className="add-form d-flex">
-                <input type="text"
-                    className="form-control new-post-label"
-                    placeholder="Что за задача?" />
-                <input type="number"
-                    className="form-control new-post-label"
-                    placeholder="Какой дэдлайн?" />
+class TaskAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            task: '',
+            date: ''
+        }
+    }
 
-                <button type="submit"
-                        className="btn btn-outline-light">Добавить</button>
-            </form>
-        </div>
-    )
+    onValueChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    };
+
+    render() {
+        const {task, date} = this.state;
+
+        return (
+            <div className="app-add-form">
+                <h3>Добавьте новую задачу</h3>
+                <form
+                    className="add-form d-flex">
+                    <input type="text"
+                        className="form-control new-post-label"
+                        placeholder="Что за задача?"
+                        name='task'
+                        value = {task}
+                        onChange={this.onValueChange} />
+                    <input type="number"
+                        className="form-control new-post-label"
+                        placeholder="Какой дэдлайн?" 
+                        name='date'
+                        value = {date}
+                        onChange={this.onValueChange}/>
+    
+                    <button type="submit"
+                            className="btn btn-outline-light">Добавить</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default TaskAddForm;
