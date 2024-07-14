0d9205bcfc5979b3f61c3e7605fffade59f399f0
valeriya <valeriyastoyan26@gmail.com>
2024-07-14 11:21:00 +0300

    corr mist

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,11 +1,11 @@
 import './app-info.css';
 
-const AppInfo = () => {
+const AppInfo = ({tasks, priorityTasks}) => {
     return (
         <div className='app-info'>
             <h1>Учёт задач на неделю</h1>
-            <h2>Общее число задач: </h2>
-            <h2>Задачи в приоритете: </h2>
+            <h2>Общее число задач: {tasks}</h2>
+            <h2>Задачи в приоритете: {priorityTasks}</h2>
         </div>
     )
 }
